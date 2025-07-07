7df366501d53639a36fce64ffa6a77b26acee997
MikeJohnshoy <mike.johnshoy@gmail.com>
2025-07-06 21:23:03 -0400

    delete old functions

--- src/cessb_limiter.h
+++ src/cessb_limiter.h
@@ -1,5 +1,2 @@
 #include <stddef.h>
-void cessb_lookahead_init(void);
-int cessb_lookahead_process(const float *new_fft_block, float *limited_block);
-int cessb_lookahead_flush(float *limited_block);
-void cessb_envelope_limiter_lookahead(const float *in, float *out, size_t len, float limit, int lookahead);
+int cessb_lookahead_process(const IQPair* cessb_in);
