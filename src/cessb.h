31a7e0ea8bf881e1906d329ef5f23ccc761a8640
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-02-06 13:41:48 -0500

    Modify AGC attack and release times
    Updated AGC parameters for attack and release times.

--- src/cessb.h
+++ src/cessb.h
@@ -21,11 +21,11 @@
 // Maximum gain cut (20 dB)
 #define AGC_MIN_GAIN 0.1f
 // AGC attack time in milliseconds (fast to catch loud passages)
-#define AGC_ATTACK_MS 10.0f
+#define AGC_ATTACK_MS 100.0f
 // AGC release time in milliseconds (slow to avoid pumping)
-#define AGC_RELEASE_MS 300.0f
+#define AGC_RELEASE_MS 100.0f
 // AGC gain smoothing time constant (very slow for smooth gain changes)
-#define AGC_GAIN_SMOOTH_MS 500.0f
+#define AGC_GAIN_SMOOTH_MS 50.0f
 
 // --- CESSB Thresholds ---
 // Clip level: initial hard clip threshold (as fraction of 1.0)
@@ -193,6 +193,7 @@ void cessb_get_stats(cessb_state_t *state, float *peak_reduction_db,
                      float *avg_power_gain_db, float *talk_power_db);
 void cessb_reset_stats(cessb_state_t *state);
 void cessb_debug_print_stats(cessb_state_t *state);
+
 
 
 
