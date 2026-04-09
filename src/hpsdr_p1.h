76ef0dc3b0fc82cae6e45c510fd7c257f8927491
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-04-09 14:49:04 -0400

    Enhance hpsdr_p1.h with TX IQ handling functions
    Added functions for handling TX IQ data from remote SDR clients.

--- src/hpsdr_p1.h
+++ src/hpsdr_p1.h
@@ -13,4 +13,12 @@ void hpsdr_send_iq(double *i_samples, double *q_samples, int n);
 void hpsdr_poll(void);
 int  hpsdr_is_connected(void);
 
+// --- TX IQ from remote SDR client (e.g., SDRConsole) ---
+// Returns 1 if a remote app is actively sending TX IQ data
+int  hpsdr_tx_iq_active(void);
+
+// Retrieve up to max_samples of 96kHz-upsampled TX IQ.
+// Returns the number of sample pairs actually written.
+int  hpsdr_get_tx_iq(double *out_i, double *out_q, int max_samples);
+
 #endif // HPSDR_P1_H
