d3d0a0d2afbe73c58b3f47b54c42bdcd9cfff015
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-04-15 16:05:25 -0400

    transmit off.

--- src/hpsdr_p1.c
+++ src/hpsdr_p1.c
@@ -348,7 +348,7 @@ static void handle_command(uint8_t *buf, int len, struct sockaddr_in *sender) {
 
         // Track remote MOX state and trigger T/R switch (from every frame)
         static int remote_mox = 0;
-        if (addr == 0 && ptt != remote_mox) {
+        if (ptt != remote_mox) {
             remote_mox = ptt;
             printf("hpsdr: remote MOX %s\n", remote_mox ? "ON" : "OFF");
         
