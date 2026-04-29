b94551e2bd5cbd237ca469a80d9d55c3d1b57d63
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-04-29 14:56:30 -0400

    fix fat finger error

--- src/hpsdr_p1.h
+++ src/hpsdr_p1.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define HPSDR_PORT 1024
-#define HPSDR_PKT_SIZE 103
+#define HPSDR_PKT_SIZE 1032
 #define SAMPLES_PER_PACKET 126
 
 // Packet types returned by hpsdr_classify()
