84838488ac9ac03c4b56acbfcfbe14ec5f368fe7
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-05-12 13:28:12 -0400

    optimize filter

--- src/hpsdr_p1.c
+++ src/hpsdr_p1.c
@@ -199,45 +199,45 @@ int hpsdr_get_tx_iq(double *out_i, double *out_q, int max_samples) {
 }
 
 // 31-tap half-band FIR coefficients (Fs=96k, Cutoff=24k)
-// note: Every other tap is 0 except for the center tap (index 15)
-static double hb_coeffs[31] = {
+// Note: Every other tap is 0 except for the center tap (index 15)
+// Made 'static const' to allow compiler optimizations
+static const double hb_coeffs[31] = {
     -0.00055,  0.0,  0.00165,  0.0, -0.00411,  0.0,  0.00877,  0.0, 
     -0.01736,  0.0,  0.03433,  0.0, -0.07612,  0.0,  0.30338,  0.5, 
      0.30338,  0.0, -0.07612,  0.0,  0.03433,  0.0, -0.01736,  0.0, 
      0.00877,  0.0, -0.00411,  0.0,  0.00165,  0.0, -0.00055
 };
 
-static double rx_hist_i[31];
-static double rx_hist_q[31];
+// History buffers increased to 32 for power-of-two masking (& 31)
+static double rx_hist_i[32];
+static double rx_hist_q[32];
 static int rx_hist_ptr = 0;
 
 // apply 24kHz LPF and decimate 2:1
 static void rx_filter_and_decimate(double i0, double i1, double q0, double q1) {
-  // We treat the incoming pair (i0, i1) as two sequential 96kHz samples
-  // Push i0/q0 into history
-  rx_hist_i[rx_hist_ptr] = i0;
-  rx_hist_q[rx_hist_ptr] = q0;
-  rx_hist_ptr = (rx_hist_ptr + 1) % 31;
-
-  // Push i1/q1 into history
+  // Push newest sample pair (i1/q1 then i0/q0)
+  rx_hist_ptr = (rx_hist_ptr - 1) & 31;
   rx_hist_i[rx_hist_ptr] = i1;
   rx_hist_q[rx_hist_ptr] = q1;
-  rx_hist_ptr = (rx_hist_ptr + 1) % 31;
 
-  // Perform the FIR convolution only ONCE (Decimation 2:1)
-  double filt_i = 0;
-  double filt_q = 0;
+  rx_hist_ptr = (rx_hist_ptr - 1) & 31;
+  rx_hist_i[rx_hist_ptr] = i0;
+  rx_hist_q[rx_hist_ptr] = q0;
+
+  // Optimized FIR convolution using symmetry and half-band properties
+  // Center tap (index 15) is 0.5
   int p = rx_hist_ptr;
+  double filt_i = rx_hist_i[(p + 15) & 31] * 0.5;
+  double filt_q = rx_hist_q[(p + 15) & 31] * 0.5;
 
-  for (int j = 0; j < 31; j++) {
-    // We iterate backwards from the current pointer
-    int idx = (p - 1 - j);
-    if (idx < 0)
-      idx += 31;
-
-    // Optimization: You could skip the 0.0 coefficients here if desired
-    filt_i += rx_hist_i[idx] * hb_coeffs[j];
-    filt_q += rx_hist_q[idx] * hb_coeffs[j];
+  // Sum symmetric non-zero taps (0, 2, 4, 6, 8, 10, 12, 14)
+  // This reduces multiplications from 31 down to 9
+  for (int j = 0; j < 15; j += 2) {
+    double c = hb_coeffs[j];
+    int idx_low = (p + j) & 31;
+    int idx_high = (p + 30 - j) & 31;
+    filt_i += (rx_hist_i[idx_low] + rx_hist_i[idx_high]) * c;
+    filt_q += (rx_hist_q[idx_low] + rx_hist_q[idx_high]) * c;
   }
 
   // Store in the HPSDR buffer
